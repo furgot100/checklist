7f7784dedf15739a976daff86585d312dd339351
furgot <frtsang40@gmail.com>
2019-09-03 13:49:35 -0700

    finishing touches

--- checklist.py
+++ checklist.py
@@ -5,7 +5,7 @@ def create(item):
 
 # READ 
 def read(index):
-    return checklist[index]
+    return checklist[int(index)]
 
 # UPDATE
 def update(index, item):
@@ -14,18 +14,18 @@ def update(index, item):
 
 # DESTROY
 def destroy(index):
-    checklist.pop(index)
+    checklist.pop(int(index))
 
 
 def list_all_items():
     index = 0
     for list_item in checklist:
-        print(str(index) + list_item)
+        print("{} {}".format(int(index), list_item))
         index += 1
 
 
 def mark_completed(index):
-     checklist[index] = "√ " + checklist[index]
+     checklist[int(index)] = "√ " + checklist[int(index)]
     
 
 
@@ -34,6 +34,7 @@ def mark_completed(index):
 def select(function_code):
     if function_code == "C":
         input_item = user_input("Input item:")
+        create(input_item)
     elif function_code == "R":
         item_index = user_input("Index Number?")
         read(item_index)
@@ -42,8 +43,8 @@ def select(function_code):
     elif function_code =="Q":
         return False
     else:
-        print("Unkown Option")        
-
+        print("Unkown Option")
+    return True        
 def user_input(prompt):
     user_input = input(prompt)
     return user_input
@@ -53,25 +54,38 @@ def user_input(prompt):
 def test():
     create("purple sox")
     create("red cloak")
+    
     print(read(0))
     print(read(1))
+    
     update(0, "purple socks")
+    
     destroy(1)
+    
     print(read(0))
+    
     list_all_items()
+    
     mark_completed(0)
+    
     print(read(0))
    
     select("C")
     list_all_items()
     select("R")
     list_all_items()
+    select("P")
+    list_all_items()
+    select("Q")
+    list_all_items()
 
     user_value = user_input("Please Enter a value:")
     print(user_value)
 
 test()
+
+
 running = True
 while running:
-    selection = user_input("Press C to add to list, T to Read from list, P to display list, and Q to quit")
-    select(selection)
+    selection = user_input("Press C to add to list, R to Read from list, P to display list, and Q to quit")
+    running = select(selection)
